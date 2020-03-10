4f8229dd268b650ba07b6ff9714f022740469b28
Evgeny <e.archibasov@gmail.com>
2020-03-11 06:29:31 +1000

    Finish Todo list in oop style

---
+++ ToDo/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index',
+
+    output: {
+        path: path.resolve(__dirname, 'public/js'),
+        filename: 'app.js',
+        publicPath: '/js'
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+    },
+
+    devtool: 'cheap-eval-source-map'
+}
