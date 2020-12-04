0b79d48b2ffc0632d85ddd16cf89ee61c4635eac
Evgeny Archibasov <e.archibasov@gmail.com>
2020-12-04 10:31:20 +1000

    Add some tips and note to tabs.js

--- JS/js/tabs.js
+++ JS/js/tabs.js
@@ -5,6 +5,7 @@ window.addEventListener('DOMContentLoaded', function() {
         tabsItem = document.querySelectorAll('.tabs__item'),
         tabContent = document.querySelectorAll('.tabcontent');
 
+    // Скрываем содержание табов за исключением первого
     function hideTabContent(a) {
         for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
@@ -14,6 +15,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     hideTabContent(1)
 
+    // Показать содержимое таба b
     function showTabContent(b) {
         if(tabContent[b].classList.contains('hide')) {
             tabContent[b].classList.remove('hide');
@@ -21,6 +23,7 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+    // Добавляем класс active нажатому табу
     function toggleTabActive(target) {
         for (let i = 0; i < tabsItem.length; i++) {
             tabsItem[i].classList.remove('active');
@@ -30,12 +33,12 @@ window.addEventListener('DOMContentLoaded', function() {
 
     tabs.addEventListener('click', function(event) {
         let target = event.target;
-        if (target && target.classList.contains('tabs__item')) {
+        if (target && target.classList.contains('tabs__item')) { // Делегирование событий табам
             for (let i = 0; i < tabsItem.length; i++) {
-                if (target === tabsItem[i]) {
-                    hideTabContent(0);
-                    showTabContent(i);
-                    toggleTabActive(target);
+                if (target === tabsItem[i]) { // Если при переборе всех табов есть совпадение на нажатый
+                    hideTabContent(0); // Скрываем содержание
+                    showTabContent(i); // Показываем тот, чей номер таба совпадает с содержимым
+                    toggleTabActive(target); // Подсвечиваем нажатый таб
                     break;
                 }
             }
