638d6748cdc0095aea31763872157c44e333d7b5
Evgeny Archibasov <e.archibasov@gmail.com>
2020-12-22 01:58:15 +1000

    Some practice with this

--- JS/js/this.js
+++ JS/js/this.js
@@ -48,7 +48,55 @@ console.log(double(5))
 const btn = document.querySelector('button');
 btn.addEventListener('click', function() { // this = button
     console.log(this)
-})
+});
 btn.addEventListener('click', () => { // this = window
     console.log(this)
-})
+});
+
+// Моментальный вызов фу-ции
+(function(str) {
+    console.log(str)
+})('hi')
+
+let test = {
+    i: 1,
+    foo: function() {
+        console.log(this) // объект
+        function foo2() {
+            console.log(this) // window. Внутренняя фу-ция не привязана к контексту объекта
+        }
+        return foo2()
+    }
+}
+console.log(test.foo());
+
+// Фу-ция конструктор
+function Country(name, traveled) {
+    this.name = name ? name : 'Russia';
+    this.traveled = Boolean(traveled);
+}
+Country.prototype.travel = function() {
+    this.traveled = true;
+    console.log(this)
+}
+
+let france = new Country('France', false);
+france.travel()
+console.log(france) // this = фу-ция конструктор с аргументами france
+
+let usa = new Country;
+usa.travel()
+console.log(usa) // this = фу-ция конструктор с аргументами usa
+console.log(france.prototype == usa.prototype) // true
+
+
+// // Контекст вызова this = Foo
+function Foo(str) {
+    console.log(this)
+    this.property = str;
+}
+
+let foo2 = new Foo('foo2');
+let foo3 = new Foo('foo3');
+console.log(foo2.property)
+console.log(foo3.property)
