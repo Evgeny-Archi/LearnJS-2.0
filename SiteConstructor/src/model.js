cfaceb4ae0f92695d51ba1f6a82947cf5998a4af
Evgeny <e.archibasov@gmail.com>
2021-03-03 04:36:43 +1000

    Added some classes and refactoring

--- SiteConstructor/src/model.js
+++ SiteConstructor/src/model.js
@@ -1,16 +1,42 @@
 import image from './assets/image.png'
+import {TitleBlock, TextBlock, ColumnsBlock, ImageBlock} from './classes/block'
+
+const text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, laborum.'
 
 export const model = [
-    { type: 'title', value: 'Конструктор сайтов', options: {
-            tag: 'h2',
-            styles: 'background: grey; color: #fff;'
-        } },
-    { type: 'text', value: 'some text' },
-    { type: 'columns', value: [
-            '1111',
-            '2222',
-            '33333',
-            '4444'
-        ] },
-    { type: 'image', value: image }
+    new TitleBlock('Конструктор сайтов', {
+        tag: 'h2',
+        styles: {
+            background: 'grey',
+            color: '#fff',
+            'text-align': 'center',
+            padding: '1.5rem'
+        }
+    }),
+    new ImageBlock(image, {
+        styles: {
+            padding: '2rem 2rem',
+            display: 'flex',
+            'justify-content': 'center'
+        },
+        imageStyles: {
+            width: '800px',
+            height: 'auto'
+        },
+        alt: 'this is image'
+    }),
+    new ColumnsBlock(['column 1', 'column 2', 'column 3'], {
+        styles: {
+            background: '#ccc',
+            color: '#fff',
+            'text-align': 'center'
+        }
+    }),
+    new TextBlock(text, {
+        tag: 'p',
+        styles: {
+            padding: '1rem',
+            border: '1px solid #ccc'
+        }
+    })
 ]
