0ad6f8ea081199bfccd19e01c559a646d8713353
Evgeny Archibasov <e.archibasov@gmail.com>
2021-04-03 19:50:52 +1000

    Add lazy load script for images

--- LazyLoad/script.js
+++ LazyLoad/script.js
@@ -1,9 +1,26 @@
 (function() {
     'use strict'
 
-    const lazyImages = document.querySelectorAll('img[data-src]')
-    const windowHeight = document.documentElement.clientHeight
+    const imgItems = document.querySelectorAll('.page__image')
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 1.0
+    }
 
-    console.log(windowHeight)
-    console.log(lazyImages[0].getBoundingClientRect(), pageYOffset)
+    function lazyScroll(entries, observer) {
+        if (entries[0].intersectionRatio <= 0) return
+        entries.forEach(entry => {
+            entry.target.querySelector('svg').remove()
+
+            const image = entry.target.querySelector('img[data-src]')
+            image.style.display = 'block'
+            image.src = image.dataset.src
+            image.removeAttribute('data-src')
+
+            observer.unobserve(entry.target)
+        })
+    }
+    const observer = new IntersectionObserver(lazyScroll, options)
+    imgItems.forEach(item => observer.observe(item))
 })()
